7fef3adb3c793b7a56e80b96a0f495731eb43d9a
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-07-04 20:22:08 +0300

    chore: Update home template to include form action and CSRF token

--- client_server/data_input/views.py
+++ client_server/data_input/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
                   
                   
 def add(request):
-    pass
+    if request.method == 'POST':
+        data = request.POST['data']
+        return HttpResponse("Data added successfully")
+    else:
+        return render(request, 'home.html')
