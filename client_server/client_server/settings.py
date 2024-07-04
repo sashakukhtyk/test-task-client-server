7c383b1d52b7aeb146c928be32eae40b31b398f0
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-07-04 18:48:46 +0300

    chore: Update database configuration to use PostgreSQL

--- client_server/client_server/settings.py
+++ client_server/client_server/settings.py
@@ -64,8 +64,11 @@ WSGI_APPLICATION = "client_server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test_client",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
     }
 }
 
